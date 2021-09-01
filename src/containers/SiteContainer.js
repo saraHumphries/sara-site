28f14d4b2addd0d602ef3f4882844979ed4a518f
sara <smhumphries@hotmail.co.uk>
2021-09-01 16:55:05 +0100

    adds second project with gif

--- src/containers/SiteContainer.js
+++ src/containers/SiteContainer.js
@@ -1,20 +1,25 @@
 import "./SiteContainer.css"
 import Project from "../components/Project";
 import vetGif from '../assets/images/vet_gif.gif';
+import testGif from '../assets/images/test_gif.gif';
 
 const SiteContainer = function() {
 
-// const vetImage = require('../assets/images/vet_gif.gif').default;
 
 const vetProject = {
     title: "Vet app",
     date: "June 2021",
     description: "A solo project using Flask/Python and an relational DB to store, update, read, delete data.",
     gitLink: "https://github.com/saraHumphries/vet_admin_app",
-    gifImage: {vetGif}
 };
 
-// console.log('image', vetProject.gifImage);
+const PsychometricTestProject = {
+    title: "Psychometric test app",
+    date: "August 2021",
+    description: "Final solo full-stack project. Java (Spring) server-side, React.js client-side.",
+    gitLink: "https://github.com/saraHumphries/psychometric_test_app",
+};
+
 
 
 
@@ -33,7 +38,7 @@ const vetProject = {
                 <section id='project-section'>
                     <div id='projects'>
                         <Project project = {vetProject} gif = {vetGif}></Project>
-                        
+                        <Project project = {PsychometricTestProject} gif = {testGif} ></Project>
                     </div>
                 </section>
             </main>
