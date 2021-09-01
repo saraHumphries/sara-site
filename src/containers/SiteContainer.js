16febcfa324c27dbdfb99b82dc392f634c46d78c
sara <smhumphries@hotmail.co.uk>
2021-09-01 17:00:09 +0100

    adds CV project with GIF

--- src/containers/SiteContainer.js
+++ src/containers/SiteContainer.js
@@ -2,6 +2,7 @@ import "./SiteContainer.css"
 import Project from "../components/Project";
 import vetGif from '../assets/images/vet_gif.gif';
 import testGif from '../assets/images/test_gif.gif';
+import coronaGif from '../assets/images/coronavirus_gif.gif';
 
 const SiteContainer = function() {
 
@@ -13,11 +14,18 @@ const vetProject = {
     gitLink: "https://github.com/saraHumphries/vet_admin_app",
 };
 
-const PsychometricTestProject = {
+const psychometricTestProject = {
     title: "Psychometric test app",
-    date: "August 2021",
+    date: "June 2021",
     description: "Final solo full-stack project. Java (Spring) server-side, React.js client-side.",
     gitLink: "https://github.com/saraHumphries/psychometric_test_app",
+};
+
+const coronavirusProject = {
+    title: "Corona virus data tracking app",
+    date: " 2021",
+    description: "A small JavaScript project using a .gov API to show the most recent covid 19 data.",
+    gitLink: "https://github.com/saraHumphries/corona_virus_tracker",
 };
 
 
@@ -38,7 +46,8 @@ const PsychometricTestProject = {
                 <section id='project-section'>
                     <div id='projects'>
                         <Project project = {vetProject} gif = {vetGif}></Project>
-                        <Project project = {PsychometricTestProject} gif = {testGif} ></Project>
+                        <Project project = {psychometricTestProject} gif = {testGif} ></Project>
+                        <Project project = {coronavirusProject} gif={coronaGif}></Project>
                     </div>
                 </section>
             </main>
